04563c581db9e5808ab0c523d4b0d2d7805406f1
Aliza05 <aliza.zia524@gmail.com>
2024-08-29 17:12:19 +0500

    folder structure is corrected

--- mysite/urls.py
+++ mysite/urls.py
@@ -18,6 +18,6 @@ from django.urls import include, path
 
 urlpatterns = [
     path('admin/', admin.site.urls),
-    path('tracker/', include('tracker.urls')),
+    path('', include('tracker.urls')),
     path('activity/', include('actstream.urls')),
 ]
